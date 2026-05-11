693f2c4729c8c8db1796306cae33b40178083d8c
Kartik Giroti <kargir17@kingshigh.school.nz>
2026-05-11 15:00:13 +1200

    finished main menu

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,11 +1,10 @@
-extends Node3D
+extends Control
 
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://maps/test_world.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_quit_button_pressed():
+	get_tree().quit()
