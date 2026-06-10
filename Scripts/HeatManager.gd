e963a7c25b720252700fa8d1eb580be12a29b131
Kartik Giroti <kargir17@kingshigh.school.nz>
2026-06-11 11:40:07 +1200

    font stuff

--- Scripts/HeatManager.gd
+++ Scripts/HeatManager.gd
@@ -11,9 +11,9 @@ var heat_value: float  = 50.0
 var max_heat: float    = 100.0
 var heat_floor: float  = 0.0
 
-@export var heat_gain_per_second: float        = 5.0
-@export var heat_loss_per_second: float        = 15.0
-@export var movement_speed_threshold: float    = 1.0
+@export var heat_gain_per_second: float        = 8.0
+@export var heat_loss_per_second: float = 25.0
+@export var movement_speed_threshold: float = 1.0
 @export_range(0.01,0.3,0.01) var gain_speed_scale_min: float = 0.02
 @export_range(1.0,5.0,0.1)   var gain_curve_power: float     = 4.0
 @export_range(1.0,12.0,0.1)  var loss_speed_scale_max: float = 7.0
